d1b9c05246d57aca0e2edd3106cdaa30fc5de8b4
Savio Resende <savio@savioresende.com.br>
2019-04-26 05:49:10 -0400

    Avatar not required.

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
         })->value);
 
         return view('home', [
-            'userAvatar' => $tribeAccountMeta->avatar
+            'userAvatar' => $tribeAccountMeta->avatar ?? ''
         ]);
     }
 }
